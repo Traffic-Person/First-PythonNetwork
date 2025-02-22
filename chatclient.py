dad3d73d27ad08c049b424ed33586849f0d02066
Traffic-Person <eryk.barczak@zsosieknadwisla.pl>
2025-02-22 15:27:56 +0100

    Add files via upload

---
+++ chatclient.py
@@ -0,0 +1,34 @@
+import threading
+import socket
+
+nickname = input("Choose a nickname: ")
+ip = '5.173.150.8'
+port = 55555
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((ip, port))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
